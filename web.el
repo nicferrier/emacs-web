658c4e7597949052dbaea49ca21079915ff88bb6
Nic Ferrier <nferrier@ferrier.me.uk>
2014-07-28 18:51:26 +0100

    slightly better description of streaming.

--- web.el
+++ web.el
@@ -30,7 +30,8 @@
 ;; This is an HTTP client using lexical scope.  This makes coding with
 ;; callbacks easier than with `url'.  This package also provides a
 ;; streaming mode where the callback is continually called whenever
-;; chunk encoding chunks are completed.
+;; data arrives. This is particularly useful for chunked encoding
+;; scenarios.
 
 ;;; Code:
 

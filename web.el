2e7ad3cb20d33c0501c3298267759ffb65d2fdde
Nic Ferrier <nferrier@ferrier.me.uk>
2014-08-27 20:25:01 +0100

    update the README that marmalade will present.

--- web.el
+++ web.el
@@ -33,6 +33,24 @@
 ;; data arrives. This is particularly useful for chunked encoding
 ;; scenarios.
 
+;; Examples:
+
+;; GET-ing an HTTP page
+;;
+;; (web-http-get
+;;  (lambda (con header data)
+;;    (message "the page returned is: %s" data))
+;;  :url "http://emacswiki.org/wiki/NicFerrier")
+
+;; POST-ing to an HTTP app
+;;
+;; (web-http-post
+;;  (lambda (con header data)
+;;    (message "the data is: %S" data))
+;;  :url "http://example.org/postplace/"
+;;  :data '(("parameter1" . "data")
+;;          ("parameter2" . "more data")))
+
 ;;; Code:
 
 ;; Style-note: This codes uses the Emacs style of:
